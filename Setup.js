e41a3ce2af3799ee87822c07ec207aa39f99d7ad
RolandWarburton <warburtonroland@gmail.com>
2020-06-25 21:04:38 +1000

    :wrench: Add check for missing uploads dir

--- Setup.js
+++ Setup.js
@@ -1,1 +1,24 @@
+const fs = require("fs");
+const dotenv = require("dotenv").config();
+
+const createDir = (path) => {
+	if (!fs.existsSync(path))
+		fs.mkdir(path, (err) => {
+			if (err) {
+				console.error(err);
+				return;
+			} else {
+				console.log(`Build directory: ${path}`);
+			}
+		});
+};
+
+if (process.env.MODE == "development") {
+	createDir(process.env.DEVELOPMENT_UPLOAD_DIRECTORY_LOCATION);
+}
+
+if (process.env.MODE == "production") {
+	createDir(process.env.PRODUCTION_UPLOAD_DIRECTORY_LOCATION);
+}
+
 console.log("Setup.js");
