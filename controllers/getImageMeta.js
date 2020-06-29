216c4496f7ed0231f3290fbbaabe9ab309d4f57c
RolandWarburton <warburtonroland@gmail.com>
2020-06-29 17:19:22 +1000

    :recycle: Improved stuff (i think)

--- controllers/getImageMeta.js
+++ controllers/getImageMeta.js
@@ -1,31 +1,29 @@
 const Image = require("../models/imageModel");
+const getImageMetaQuery = require("../dbQueries/queryImageMeta");
 const debug = require("debug")("imageHost:controllers");
 require("dotenv").config();
 
 // * get a single image
 const getImageMeta = (req, res) => {
 	debug("Running getImageMeta...");
-	Image.findOne({ _id: req.params.id }, "_id tags", (err, image) => {
-		if (err) {
-			return res.status(400).json({ success: false, error: err });
-		}
 
-		if (!image) {
-			return res
-				.status(404)
-				.json({ success: false, error: `Image not found` });
-		} else {
-			res.status(200).json({
-				success: true,
-				data: {
-					_id: image._id,
-					path: image.path,
-					tags: image.tags,
-					imageURL: req.headers.host + "/image/" + image._id,
-				},
-			});
-		}
-	}).catch((err) => console.log(err));
+	getImageMetaQuery(req.params.id)
+		.then((imageMeta) => {
+			if (imageMeta) {
+				// return the image meta ✅
+				debug(`image meta returned`);
+				return res.status(200).json(imageMeta);
+			} else {
+				// something went wrong ⛔
+				debug(`ERROR: failed to get image meta`);
+				return res
+					.status(404)
+					.json({ success: false, error: `Image not found` });
+			}
+		})
+		.catch((err) => {
+			debug(`ERROR: Something went wrong: ${err}`);
+		});
 };
 
 module.exports = getImageMeta;
---
+++ dbQueries/queryImageMeta.js
@@ -0,0 +1,42 @@
+const Image = require("../models/imageModel");
+const debug = require("debug")("imageHost:query");
+require("dotenv").config();
+
+// * get a single images meta
+/** Returns a promise that resolves to an object from the imageHost.imageHost database
+ * or undefined if not found
+ * @example
+ * queryImageMeta("55d6800f-43ff-5519-aba6-c100a0e9cad9");
+ * @param {string} id - mongoDB _id of object to lookup
+ */
+const queryImageMeta = async (id) => {
+	debug("Running getImageMeta from db queries...");
+	try {
+		return Image.findOne({ _id: id }, "_id tags path", (err, image) => {
+			if (err) {
+				debug(`ERROR retrieving image meta from mongo ${err}`);
+				return undefined;
+			}
+
+			if (!image) {
+				debug("ERROR no image found");
+				return undefined;
+			} else {
+				debug("Returning image meta");
+				const data = {
+					success: true,
+					data: {
+						_id: image._id,
+						path: image.path,
+						tags: image.tags,
+					},
+				};
+				return data;
+			}
+		});
+	} catch (err) {
+		return debug(`ERROR async block failed ${err}`);
+	}
+};
+
+module.exports = queryImageMeta;
