6b2232011f8b120b2434189661b7780932868ade
RolandWarburton <warburtonroland@gmail.com>
2020-06-30 17:36:41 +1000

    :construction: ADD debugging to auth middleware

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,21 +1,31 @@
 const jwt = require("jsonwebtoken");
+const debug = require("debug")("imageHost:middleware");
 require("dotenv");
 
 const authenticate = (req, res, next) => {
+	debug(`running authenticate middleware`);
+
 	// check the auth-token given to the user after they log in
 	const token = req.header("auth-token");
+	debug(`Got token from header: ${token.substring(0, 6)}...`);
 
 	if (!token) {
+		debug(`Token is empty. Returning 401`);
 		return res
 			.status(401)
 			.json({ success: false, error: "Unauthorized to do this D:" });
 	}
 
 	try {
+		// decode the token and check it with jwt
 		const verified = jwt.verify(token, process.env.USER_KEY);
+		debug(`Token verified as user: ${JSON.stringify(verified)}`);
+
+		// set the user to verified = true or false
 		req.user = verified;
 		next();
 	} catch (err) {
+		debug(`Token exxists but is invalid. Returning 400`);
 		return res.status(400).json({ success: false, error: "Invalid token" });
 	}
 };
