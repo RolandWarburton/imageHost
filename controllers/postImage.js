1c8df18096dd9d4f7981dfb84fefc0d97ccf280a
RolandWarburton <warburtonroland@gmail.com>
2020-07-01 23:41:40 +1000

    :construction: Got fields working

--- controllers/postImage.js
+++ controllers/postImage.js
@@ -6,15 +6,16 @@ const { v5: uuidv5 } = require("uuid");
 const mime = require("mime-types");
 const sizeOf = require("image-size");
 const fs = require("fs");
+const util = require("util");
 const debug = require("debug")("imageHost:controllers");
 
 require("dotenv").config();
 
 const filetypes = ["png", "jpg", "jpeg", "svg", "gif", "mp4", "webm"];
 
-const getFilepathDetails = (mimetype) => {
+const getFilepathDetails = (mimetype, uuid) => {
 	// make a random filename
-	const filehash = uuidv5(Math.random().toString(), uuidv5.URL);
+	const filehash = uuid;
 
 	// get the extension
 	const fileExtension = mime.extension(mimetype);
@@ -32,28 +33,91 @@ const getFilepathDetails = (mimetype) => {
 	};
 };
 
-const postImage = async (req, res) => {
+const getFileDimensions = (file) => {
+	debug(sizeOf(file));
+};
+
+const getdim = util.promisify(sizeOf);
+
+const postImage = (req, res) => {
 	debug("Running postImage...");
 
+	const image = new Image();
+	let postedImage = false;
+	const UUID = uuidv5(Math.random().toString(), uuidv5.URL);
+
+	// 100MB filesize limit
 	const busboy = new Busboy({
 		headers: req.headers,
 		limits: {
 			files: 1,
+			fileSize: 100 * 1024 * 1024,
 		},
 	});
 
+	// * ============================== On File ==============================
 	busboy.on("file", function (fieldname, file, filename, encoding, mimetype) {
-		const { saveFilepath, fileExtension } = getFilepathDetails(mimetype);
+		const { saveFilepath, fileExtension } = getFilepathDetails(
+			mimetype,
+			UUID
+		);
+		// getFileDimensions(file);
 		// if (fieldname != "image")
 		if (filetypes.includes(fileExtension)) {
-			file.pipe(fs.createWriteStream(saveFilepath));
+			debug("saving...");
+			postedImage = true;
+			image.path = saveFilepath;
+
+			const f = fs.createWriteStream(saveFilepath);
+			file.pipe(f);
+			// f.on("finish", () => {
+			// });
 		} else {
-			res.status(401).json({ success: false, error: "u did a bad!" });
+			res.status(400).json({
+				success: false,
+				error: "Wrong image format",
+			});
 			res.end();
 		}
 	});
 
-	busboy.on("finish", function () {
+	// * ============================== On Field ==============================
+	busboy.on("field", function (
+		fieldname,
+		val,
+		fieldnameTruncated,
+		valTruncated,
+		encoding,
+		mimetype
+	) {
+		// stick this field onto the image object
+		image[fieldname] = val;
+	});
+
+	// * ============================== On Finish ==============================
+	busboy.on("finish", async () => {
+		if (!postedImage) {
+			// respond with 400 ❌
+			res.status(400).json({
+				success: false,
+				error: "No image provided.",
+			});
+		}
+
+		// set meta
+		image.meta.uploadDate = new Date();
+		image.meta.uploadMonth = new Date().getMonth();
+		image.meta.uploadYear = new Date().getUTCFullYear();
+
+		// get the dimension meta
+		image.meta.dimensions = await getdim(image.path);
+
+		// set the id
+		image._id = UUID;
+
+		debug(image);
+
+		// respond all good ✅
 		res.status(200).json({ success: true });
 		res.end();
 	});
