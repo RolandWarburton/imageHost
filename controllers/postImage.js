34c456d83ac2e134e361148d570bd70b20450615
RolandWarburton <warburtonroland@gmail.com>
2020-07-02 00:13:43 +1000

    :construction: Can post to database

--- controllers/postImage.js
+++ controllers/postImage.js
@@ -11,7 +11,9 @@ const debug = require("debug")("imageHost:controllers");
 
 require("dotenv").config();
 
-const filetypes = ["png", "jpg", "jpeg", "svg", "gif", "mp4", "webm"];
+// txt = svg
+const imageFiletypes = ["gif", "ico", "bmp", "jpeg", "png", "txt", "svg"];
+const mediaFiletypes = ["mp4", "webm"];
 
 const getFilepathDetails = (mimetype, uuid) => {
 	// make a random filename
@@ -56,43 +58,50 @@ const postImage = (req, res) => {
 	});
 
 	// * ============================== On File ==============================
-	busboy.on("file", function (fieldname, file, filename, encoding, mimetype) {
+	busboy.on("file", (fieldname, file, filename, encoding, mimetype) => {
 		const { saveFilepath, fileExtension } = getFilepathDetails(
 			mimetype,
 			UUID
 		);
-		// getFileDimensions(file);
-		// if (fieldname != "image")
-		if (filetypes.includes(fileExtension)) {
-			debug("saving...");
-			postedImage = true;
-			image.path = saveFilepath;
 
-			const f = fs.createWriteStream(saveFilepath);
-			file.pipe(f);
-			// f.on("finish", () => {
-			// });
-		} else {
+		if (
+			!imageFiletypes.includes(fileExtension) &&
+			!mediaFiletypes.includes(fileExtension)
+		) {
 			res.status(400).json({
 				success: false,
 				error: "Wrong image format",
 			});
 			res.end();
 		}
+
+		postedImage = true;
+		image.path = saveFilepath;
+
+		const f = fs.createWriteStream(saveFilepath);
+		file.pipe(f);
+		f.on("finish", () => {
+			debug(`saved image to filesystem`);
+		});
 	});
 
 	// * ============================== On Field ==============================
-	busboy.on("field", function (
-		fieldname,
-		val,
-		fieldnameTruncated,
-		valTruncated,
-		encoding,
-		mimetype
-	) {
-		// stick this field onto the image object
-		image[fieldname] = val;
-	});
+	busboy.on(
+		"field",
+		(
+			fieldname,
+			val,
+			fieldnameTruncated,
+			valTruncated,
+			encoding,
+			mimetype
+		) => {
+			// stick this field onto the image object
+			image[fieldname] = val;
+			// set the mime type
+			image.meta.mime = mimetype;
+		}
+	);
 
 	// * ============================== On Finish ==============================
 	busboy.on("finish", async () => {
@@ -109,26 +118,39 @@ const postImage = (req, res) => {
 		image.meta.uploadMonth = new Date().getMonth();
 		image.meta.uploadYear = new Date().getUTCFullYear();
 
+		const extension = mime.extension(image.meta.mime);
+
+		debug(extension);
+
 		// get the dimension meta
-		image.meta.dimensions = await getdim(image.path);
+		if (imageFiletypes.includes(extension)) {
+			// its a static image. get dims using the conventional way
+			const dim = await getdim(image.path);
+			image.meta.dimensions = { width: dim.width, height: dim.height };
+		}
+		if (mediaFiletypes.includes(extension)) {
+			// its a webm or mp4. idk how to do these yet 😧
+			image.meta.dimensions = { width: undefined, height: undefined };
+		}
 
 		// set the id
 		image._id = UUID;
 
 		debug(image);
 
+		image.save().then((document) => {
+			debug("saved image to database");
+			debug(document);
+		});
+
 		// respond all good ✅
-		res.status(200).json({ success: true });
+		res.status(200).json({ success: true, data: image });
 		res.end();
 	});
 
 	// Connects this read stream to busboy WriteStream to write the file and do other things
 	// https://nodejs.org/docs/v0.4.8/api/streams.html#stream.pipe
 	return req.pipe(busboy);
-
-	// generate a random ID to use the the filename and the _id
-	// const UUID = uuidv5(Math.random().toString(), uuidv5.URL);
-	// const extension = mime.extension(part.type);
 };
 
 module.exports = postImage;
