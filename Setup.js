c7eb5f6cd31f54c9e7cfce4523bb057fd9e8f616
RolandWarburton <warburtonroland@gmail.com>
2020-07-05 13:58:48 +1000

    :construction: Fix setup to work with prev changes

--- Setup.js
+++ Setup.js
@@ -3,16 +3,22 @@ const path = require("path");
 const dotenv = require("dotenv").config();
 const { User } = require("./models/userModel");
 const debug = require("debug")("imageHost:setup");
-const db = require("./database");
 const mongoose = require("mongoose");
 const chalk = require("chalk");
 const queryUser = require("./dbQueries/queryUser");
 
-// Tell the user if theres an issue with the connection
-db.on("error", console.error.bind(console, "MongoDB connection error:"));
+// get the server
+const server = require("./server");
 
-// once connected try and find the AccountMaster account. If it doesnt exist create it
-db.once("open", () => {
+// get the db
+const { db, connectToDB } = require("./database");
+connectToDB(process.env.DB_CONNECTION);
+
+db.once("open", async () => {
+	const ip = "localhost";
+	const port = process.env.PORT || 2020;
+	server.startServer(`server is now running on http://www.${ip}:${port}!`);
+
 	queryUser("AccountMaster")
 		.then((user) => {
 			if (user) {
