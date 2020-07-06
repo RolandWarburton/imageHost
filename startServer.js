036a0de17c251b22c8229354624472c826d94954
RolandWarburton <warburtonroland@gmail.com>
2020-07-06 14:15:05 +1000

    :art: CLEAN up startServer process

--- startServer.js
+++ startServer.js
@@ -1,23 +1,37 @@
 const chalk = require("chalk");
-const express = require("express");
-const path = require("path");
+const internalIp = require("internal-ip");
 require("dotenv");
 
 const server = require("./server");
-const { db, connectToDB } = require("./database");
+const { db, connectToDB, disconnectFromDB } = require("./database");
 
+/** return a messages telling the user where
+ * the server is running locally
+ * @example console.log(await getMessage());
+ */
+const getMessage = async () => {
+	const ip = await internalIp.v4();
+	const port = process.env.PORT;
+	return chalk.green(`Server is running http://${ip}:${port}`);
+};
+
+/** Call me to run the server!
+ * @example go()
+ */
 const go = async () => {
 	await connectToDB(process.env.DB_CONNECTION);
 	await server.startServer();
-	// await server.stopServer();
-	// db.close();
-	// console.log("db has stopped");
-	// server.app.use(express.static("./public"));
-
-	// debug route
-	server.app.use(
-		"/static",
-		express.static(path.resolve(process.env.ROOT, "public"))
-	);
+	console.log(await getMessage());
 };
 go();
+
+// ? To shut down
+// await server.stopServer();
+// db.disconnectFromDB();
+// console.log("db has stopped");
+
+// ? debug route
+// server.app.use(
+// 	"/static",
+// 	express.static(path.resolve(process.env.ROOT, "public"))
+// );
