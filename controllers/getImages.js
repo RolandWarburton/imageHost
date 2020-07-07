b93cbacf2934491b35b6f68020689b204734fd5c
RolandWarburton <warburtonroland@gmail.com>
2020-07-07 21:44:16 +1000

    :sparkles: Add search by tag. #7

--- controllers/getImages.js
+++ controllers/getImages.js
@@ -3,22 +3,28 @@ const mongoose = require("mongoose");
 const debug = require("debug")("imageHost:controllers");
 require("dotenv").config();
 
-// * get all images
-const getImages = (req, res) => {
-	debug("Running getImages...");
+// =============================================
+// =============== Valid Queries ===============
+// =============================================
+// ?page=N - The page number to go to
+// ?limit=N - The number of items per page
+// ?tag=String
 
-	const queries = req.query;
-	debug(`Received queries ${JSON.stringify(queries)}`);
-
+/** Return the number of items that need to be skipped
+ * to get to page number
+ * @param {number} page - The queries.page value (?page=N)
+ * @param {number} limit - The queries.limit value (?limit=N)
+ */
+const getSkipCount = (page, limit) => {
 	// get the limit query and convert it to a number to set the numberOfItemsPerPage
-	const limit = Number(queries.limit);
+	limit = Number(limit);
 
 	// Set the number of items per page to the ?limit=n if its between 10 and 100
 	const numberOfItemsPerPage =
 		limit && limit >= 10 && limit <= 100 ? limit : 20;
 
 	//get the page number and convert it to a number
-	const pageNumber = Number(queries.page);
+	const pageNumber = Number(page);
 
 	// Internally controlls how many values to skip to give the illusion of pages
 	skipCount = pageNumber ? numberOfItemsPerPage * pageNumber : 0;
@@ -27,39 +33,69 @@ const getImages = (req, res) => {
 		`Internally skipping ${skipCount} results to get to page ${pageNumber}`
 	);
 
-	Image.find(
-		{},
-		"meta.uploadDate id tags",
-		{
+	return skipCount;
+};
+
+const getLimit = (limit) => {
+	limit = Number(limit);
+	if (limit <= 20) {
+		return limit;
+	} else {
+		return 20;
+	}
+};
+
+// * get all images
+const getImages = async (req, res) => {
+	debug("Running getImages...");
+
+	const filters = {};
+	const queries = req.query;
+	debug(`Received queries ${JSON.stringify(queries)}`);
+
+	// Internally controlls how many values to skip to give the illusion of pages
+	const skipCount = getSkipCount(queries.page, queries.limit);
+
+	// ?limit
+	// how many items per page
+	const limit = getLimit(queries.limit);
+
+	// ?tags
+	// return just images with this tag. may be an array if theres multiple images
+	const tags = queries.tag;
+
+	// if there are tags, append them to the query filters
+	if (tags) filters.tags = { $in: tags };
+
+	const tagTest = ["a", "b", "c"];
+	// debug(tag);
+	try {
+		// get all the images
+		const images = await Image.find(filters, "meta.uploadDate id tags", {
 			sort: "meta.uploadDate",
 			skip: skipCount,
-			limit: numberOfItemsPerPage,
-		},
-		(err, images) => {
-			// if there is a general error
-			if (err) {
-				return res.status(400).json({ success: false, error: err });
-			}
+			limit: limit,
+		});
 
-			if (!images.length) {
-				return res.status(404).json({
-					success: false,
-					error:
-						"Images not found. Perhaps the database is empty, or you have specified a page number thats too high?",
-				});
-			}
-
-			// images.meta.uploadDate = new Date(parseInt(images.meta.uploadDate));
-			for (img of images) {
-				const t = new Date(img.meta.uploadDate).toISOString();
-				img.test = new mongoose.Types.ObjectId().getTimestamp();
-			}
-
-			images[0].test = new mongoose.Types.ObjectId().getTimestamp();
-
-			return res.status(200).json({ success: true, data: images });
+		if (!images.length) {
+			return res.status(404).json({
+				success: false,
+				error:
+					"Images not found. Perhaps the database is empty, or you have specified a page number thats too high?",
+			});
 		}
-	).catch((err) => console.log(err));
+
+		// images.meta.uploadDate = new Date(parseInt(images.meta.uploadDate));
+		for (img of images) {
+			const t = new Date(img.meta.uploadDate).toISOString();
+			img.test = new mongoose.Types.ObjectId().getTimestamp();
+		}
+		return res.status(200).json({ success: true, data: images });
+	} catch (err) {
+		return res.status(400).json({ success: false, error: err });
+	}
 };
 
 module.exports = getImages;
+
+// images[0].test = new mongoose.Types.ObjectId().getTimestamp();
