0d706f1fe072a1946ad2a67ca867722de99d9a68
jimuskin <jimuskin1234567890@gmail.com>
2020-07-10 15:26:50 +1000

    :bug: FIX server starting causing infinite program loop in setup

--- Setup.js
+++ Setup.js
@@ -6,49 +6,42 @@ const debug = require("debug")("imageHost:setup");
 const mongoose = require("mongoose");
 const chalk = require("chalk");
 const queryUser = require("./dbQueries/queryUser");
-
-// get the server
-const server = require("./server");
-
 // get the db
-const { db, connectToDB } = require("./database");
+const { db, connectToDB, disconnectFromDB } = require("./database");
+
 connectToDB(process.env.DB_CONNECTION);
 
 db.once("open", async () => {
 	const ip = "localhost";
 	const port = process.env.PORT || 2020;
-	await server.startServer(`server is now running on http://www.${ip}:${port}!`);
 
 	const username = process.env.ACCOUNT_MASTER_USERNAME;
 	const password = process.env.ACCOUNT_MASTER_PASSWORD;
 
 	const queriedUser = await queryUser("username", username);
 
-	if (queriedUser) {
-		debug(`Account with the name ${username} already exists.`);
-		return;
+	if (!queriedUser) {
+		debug(`Creating AccountMaster account (${username})`);
 
-	} else {
-		debug(`Creating AccountMaster (${username}) account...`);
-		
 		const newUser = new User({
 			username: username,
 			password: password,
 			superUser: true,
 		});
 
-		// save the account into the database and close the connection
-		await newUser.save().then(() => {
-			mongoose.disconnect();
-			return;
+		await newUser
+		.save()
+		.then((user) => {
+			debug(`Successfully created new master user: ${user.username}`);
+		}).catch((err) =>{
+			console.log(chalk.red(err));
 		});
+	}else{
+		debug(`Account already exists (${queriedUser.username})`);
 	}
 
 	// nothing more to do
-	debug("no new user needed, closing the connection");
-	mongoose.disconnect().then(() => {
-		debug("Closed the database connection");
-		return;
+	disconnectFromDB().then(() => {
+		debug("Successfully closed the database connection.");
 	});
-	
 });
